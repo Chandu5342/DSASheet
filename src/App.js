2065d3243f442aca6a291b25f75e82f81ff6be24
Chandu5342 <karrichandu309@gmail.com>
2025-03-23 11:31:42 +0530

    Updated HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './LoginRelated/Login';
-import Register from './LoginRelated/Register'
+import Register from './LoginRelated/Register';
 import React, { useEffect, useState } from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Profile from './Dashboard/Profile';
@@ -19,33 +14,30 @@ import RoleDashboard from './Role/RoleDashboard';
 import ProblemsR from './Role/ProblemsR';
 
 function App() {
-  let [user,setUser]=useState(null);
-  useEffect(()=>{
-         auth.onAuthStateChanged((user)=>{
-          setUser(user);
-         })
-  },[]);
+  let [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-         <Routes>
-            <Route path='/' element={user?<Profile></Profile>:<Login></Login>}></Route>
-            <Route path='/LoginRelated/login' element={<Login></Login>}></Route>
-            <Route path='/LoginRelated/register' element={<Register></Register>}></Route>
-            <Route path='/Dashboard/profile' element={<Profile></Profile>}></Route>
-            <Route path='/Dashboard/AddNewProb' element={<AddNewProb></AddNewProb>}></Route>
-            <Route path='/Dashboard/problems' element={<Problems></Problems>}></Route>
-            <Route path='/Role/RoleDashboard' element={<RoleDashboard></RoleDashboard>}></Route>
-            <Route path='/Role/ProblemsR' element={<ProblemsR></ProblemsR>}></Route>
-         </Routes>
-      </BrowserRouter>
-     
-     <ToastContainer/>  
-
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={user ? <Profile /> : <Login />} />
+          <Route path='/LoginRelated/login' element={<Login />} />
+          <Route path='/LoginRelated/register' element={<Register />} />
+          <Route path='/Dashboard/profile' element={<Profile />} />
+          <Route path='/Dashboard/AddNewProb' element={<AddNewProb />} />
+          <Route path='/Dashboard/problems' element={<Problems />} />
+          <Route path='/Role/RoleDashboard' element={<RoleDashboard />} />
+          <Route path='/Role/ProblemsR' element={<ProblemsR />} />
+        </Routes>
+      </HashRouter>
+      <ToastContainer />
     </>
-
-    
-
   );
 }
 
